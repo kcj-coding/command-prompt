b5c47063d45dad1fc7f8bd6435645800c280f8e5
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2026-03-28 11:48:16 +0000

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,4 @@
+Start "" /D "C:\Windows\System32" "cleanmgr.exe"
+Start "" /D "C:\Windows\System32" "services.msc"
+%SystemRoot%\explorer.exe "C:\"
+Start "" /D "C:\Program Files\Google\Chrome\Application" "chrome.exe" -incognito "www.bbc.co.uk"
