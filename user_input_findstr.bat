c9efe91ec12f3cfb577608c11fe732e54302073b
Kelvin Chilvers-Jones <119540748+kcj-coding@users.noreply.github.com>
2026-04-20 21:48:45 +0100

    Add files via upload

---
+++ user_input_findstr.bat
@@ -0,0 +1,14 @@
+@ echo off 
+rem hides text display unless instructed to display
+rem take user input set/p
+rem do something
+
+:: prompt user for input
+set /p userInput="Enter folder to search: "
+set /p textInput="Enter text to search: "
+
+rem search for text in folder
+
+rem output results to file not the command prompt for python and txt files
+cd %userInput%
+findstr /i /m /c:%textInput% *.py *.txt > C:\file_ui.txt
